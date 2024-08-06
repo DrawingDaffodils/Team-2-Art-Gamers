fc94376c07d7c9a6d0f7fd962a27b102a2106fe4
Artrix9095 <sefu786@outlook.com>
2024-08-06 16:26:46 -0700

    Change port number

--- server.py
+++ server.py
@@ -357,4 +357,4 @@ if __name__ == '__main__':
     # Start websocket server
     app.on_shutdown.append(cleanup_background_tasks)
     # app.on_startup.append(start_background_tasks)
-    web.run_app(app)
+    web.run_app(app, port=9000)
